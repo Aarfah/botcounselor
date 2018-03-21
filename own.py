36eb71c2c12cc110f764755a1f902fc1cb292c56
aarfah <aarfah786@yahoo.com>
2018-03-21 21:40:59 +0530

    First commit

---
+++ own.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import pymysql.cursors
+import pymysql
+
+def createtable():
+
+	# Open database connection
+	db = pymysql.connect("localhost","root","","counselor" )
+
+	# prepare a cursor object using cursor() method
+	cursor = db.cursor()
+
+	# Drop table if it already exist using execute() method.
+	cursor.execute("DROP TABLE IF EXISTS usermsg")
+
+	# Create table as per requirement
+	sql = """CREATE TABLE usermsg (
+	   message  VARCHAR(100))"""
+
+	cursor.execute(sql)
+
+	# disconnect from server
+	db.close()
