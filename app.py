e805071ef72f8c3b74522e969743644341fd9b7b
aarfah <aarfah786@yahoo.com>
2018-04-01 00:36:07 +0530

    latest

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os, sys
 from flask import Flask, request
 from utils import wit_response
 from pymessenger import Bot
-import mysql.connector
 
 #initialise Flask app
 app = Flask(__name__)
@@ -10,7 +9,7 @@ app = Flask(__name__)
 PAGE_ACCESS_TOKEN = "EAAFGnmzStNcBAIVibLcyq18ZBXpgoNeeQSDhYq9qjcKiIGkvtEZB0ZCUVGyxuYg5bl6qeYiJB9apFXem2A1C7ZCyKrGHuxEKQsvD85bkXlkmHLCRIwRxyDhEZANMwTxoX6VU2co6Jm6LBpvA3p9a7WF614BgZBzHLOYukYZCzBKutF9TCEH94iv"
 
 bot = Bot(PAGE_ACCESS_TOKEN)
-i=0
+
 
 
 @app.route('/', methods=['GET'])
@@ -48,15 +47,7 @@ def webhook():
 					
 					response = None
 					entity, value = wit_response(messaging_text)
-					if entity == "daytype":
-						response = "Why did you have a {} day? Do you need counseling service".format(str(value))
-					elif entity == "name":
-						response = "Hello! My name is Mitra."
-					elif entity == "sayhello":
-						response = "Hello there, I'm Mitra! How was your day?"
-					elif entity == "location":
-						response = "So how is {}?".format(str(value))
-					elif entity == "ans":
+					if messaging_text == "Hello" or "hello" or "hey" or "Hey" or "HELLO" or "HEY" or "heya":
 						response = "Are you ready for answering?"				
 						buttons =	[
 										{
